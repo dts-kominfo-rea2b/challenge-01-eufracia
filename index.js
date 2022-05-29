7ec329eecbf9cc5bf9b56c9a5edaf937ac8e20d8
eufracia <ziporalicia@gmail.com>
2022-05-29 17:10:09 +0700

    Adding data structure for users

--- index.js
+++ index.js
@@ -1,20 +1,92 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const favoriteColorMonica = new Set(["Yellow", "Pink", "White", "Purple"]);
+const favoriteRestoranMonica = new Set([
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Bento",
+  "Eggy",
+  "Padang",
+  "Tteok",
+  "Sushi",
+  "Sushi",
+]);
+
+const favoriteColorWendy = new Set(["Blue", "Black", "Grey"]);
+const favoriteRestoranWendy = new Set([
+  "Tempura",
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Padang",
+  "Katsu",
+  "Geprek",
+  "Pancake",
+  "Eggy",
+]);
+
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColorMonica,
+  isHavePet: "Yes",
+  education: [
+    {
+      name: "SD 01",
+      city: "Jakarta",
+      graduate: 2016,
+    },
+    {
+      name: "SMP 02",
+      city: "Jakarta",
+      graduate: 2019,
+    },
+    { name: "SMA 03", city: "Tangerang" },
+  ],
+  favoriteRestoranMonica,
+};
+
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColorWendy,
+  isHavePet: "No",
+  education: [
+    {
+      name: "SD 02",
+      city: "Jakarta",
+      graduate: 2010,
+    },
+    { name: "SMP 03", city: "Bogor", graduate: 2013 },
+    {
+      name: "SMA 01",
+      city: "Surabaya",
+      graduate: 2013,
+    },
+    { name: "Universitas Maju", city: "Tangerang" },
+  ],
+  favoriteRestoranWendy,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
